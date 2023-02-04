75e7df522efc57785df7d460038184d4f239e159
acgist <289547414@qq.com>
2023-02-04 16:40:50 +0800

    [+] 大小写

---
+++ taoyao-media-server/src/Signal.js
@@ -0,0 +1,21 @@
+/**
+ * 信令
+ * 1. 终端媒体流向
+ * 2. 处理音频视频：降噪、水印等等
+ */
+
+class Signal {
+
+    /**
+     * 处理事件
+     * 
+     * @param {*} message 消息
+     * @param {*} session websocket
+     */
+    on(message, session) {
+
+    }
+
+}
+
+module.exports = Signal;
