1c1408db1af10c6488f086f5d76a9c9f70c1d061
acgist <289547414@qq.com>
2023-08-18 07:48:09 +0800

    [*] 日常优化

--- taoyao-signal-server/taoyao-signal/src/main/java/com/acgist/taoyao/signal/service/UsernamePasswordService.java
+++ taoyao-signal-server/taoyao-signal/src/main/java/com/acgist/taoyao/signal/service/UsernamePasswordService.java
@@ -3,12 +3,14 @@ package com.acgist.taoyao.signal.service;
 /**
  * 帐号密码认证服务
  * 
+ * 用于验证帐号密码，如果没有提供实现默认使用配置中的帐号密码。
+ * 
  * @author acgist
  */
 public interface UsernamePasswordService {
 
     /**
-     * 认证
+     * 帐号密码认证
      * 
      * @param username 帐号
      * @param password 密码
