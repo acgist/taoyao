95398a0f6442b293e7924d8b45574f475db4204a
acgist <289547414@qq.com>
2025-09-28 18:02:10 +0800

    [+] 配置

--- taoyao-signal-server/taoyao-signal/src/main/java/com/acgist/taoyao/signal/configuration/WebSocketSignalAutoConfiguration.java
+++ taoyao-signal-server/taoyao-signal/src/main/java/com/acgist/taoyao/signal/configuration/WebSocketSignalAutoConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import com.acgist.taoyao.signal.client.websocket.WebSocketSignal;
 
@@ -27,6 +28,15 @@ public class WebSocketSignalAutoConfiguration {
     @ConditionalOnMissingBean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
+    }
+    
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        final ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxSessionIdleTimeout(60L * 1000);
+        container.setMaxTextMessageBufferSize(1024 * 1024);
+        container.setMaxBinaryMessageBufferSize(1024 * 1024);
+        return container;
     }
     
 }
